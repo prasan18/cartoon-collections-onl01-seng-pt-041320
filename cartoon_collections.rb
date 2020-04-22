e4bffe5f7569ae43fd994dbce117d41da3348a58
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-22 14:32:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+    if names.length == 0
+      puts "The line is currently empty."
+    else
+      new_array = []
+      names.each_with_index { |name, i| new_array << "#{i + 1}. #{name}"}
+        puts  "The line is currently: " << new_array.join(" ")
+    end 
+
+def summon_captain_planet(veggies)
+  veggies = %w[carrot cucumber pepper]
+   veggies.class  { |veggies, i| new_array << "#{i + 1}. #{veggies}"}
+        puts  "The line is currently: " << veggies.length(" ") 
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
